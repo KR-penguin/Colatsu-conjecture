3ed6a25f7a4f90c723ade0dcdf8182e585f88b8c
KoPeng <89641137+KR-penguin@users.noreply.github.com>
2022-09-29 06:40:45 +0900

    Add files via upload

---
+++ Colatsu conjecture/Colatsu conjecture/Colatsu conjecture.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+void Colats(unsigned long a)
+{
+    while (a != 1)
+    {
+        if (a % 2 == 0)
+        {
+            cout << a << endl;
+            a /= 2;
+        }
+        else if (a % 2 == 1)
+        {
+            cout << a << endl;
+            a *= 3;
+            a++;
+        }
+    }
+    cout << "\n\n------결과값이 1이 나왔습니다.------\n\n";
+}
+
+int main() {
+
+    std::ios_base::sync_with_stdio(false);
+    
+    unsigned long long num = 1;
+
+    unsigned long long max = 0;
+
+    cout << "몇 수까지 콜라스 추측을 해보시겠습니까? (0 ~ 18,446,744,073,709,551,615):";
+    cin >> max;
+
+    while (1)
+    {
+        if (num >= max + 1)
+        {
+            return 0;
+        }
+
+        cout << "대입할 수 : " << num << "\n\n";
+
+        Colats(num);
+
+        num++;
+    }
+
+    return 0;
+}
